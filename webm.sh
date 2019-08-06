24fd29499788b992cfb2ffd289eee1c758654746
Alexey Abdusamatov <alexey@Alexeys.ZEN.LOCAL>
2019-08-06 15:29:50 +1000

    added start stop parameters

--- webm.sh
+++ webm.sh
@@ -1,36 +1,58 @@
 inputVideo=$1
 if [[ -z $2 ]]; then
-  bitrate='1000k'
+  bitrate='3000k'
 else
   bitrate=$2'k'
 fi
-# preset='placebo'
-preset='veryslow'
+preset='placebo'
+# preset='veryslow'
 # tune='animation'
 tune='film'
 loglevel='warning'
 # loglevel='debug'
 lutFile="$3"
+
+seekToSecond=03:19.800
+# totalTime=02:27.68
+# -ss 00:23:00
+# Note that you can use two different time unit formats: sexagesimal 
+# (HOURS:MM:SS.MILLISECONDS, as in 01:23:45.678), or in seconds. If a fraction is used, such as 02:30.05, this is interpreted as "5 100ths of a second", 
+# not as frame 5. For instance, 02:30.5 would be 2 minutes, 30 seconds, and a half a second, which would be the same as using 150.5 in seconds. 
+framesv=5
+
 profile='-profile:v high -level 4.0'
 outputVideo="${inputVideo%.*}_b$bitrate.mp4"
+
+start=( '-ss' "$seekToSecond"  )
+
 if [[ -z $3 ]]; then
   # no lut
+  filters=('format=pix_fmts=rgb24,scale=-2:720,format=pix_fmts=yuv420p')
+  # filters=('format=pix_fmts=yuv420p')
   # filters=('format=pix_fmts=rgb24,scale=-2:720,format=pix_fmts=yuv420p')
-  filters=('mpdecimate,setpts=PTS,format=pix_fmts=rgb24,scale=-2:720,format=pix_fmts=yuv420p')
+  # filters=('mpdecimate,setpts=PTS,format=pix_fmts=rgb24,scale=-2:720,format=pix_fmts=yuv420p')
 else
   # lut
   filters=('format=pix_fmts=rgb24,lut3d=file='"$lutFile"',scale=-1:720,format=pix_fmts=yuv420p')
 fi
 # https://trac.ffmpeg.org/wiki/FilteringGuide
-additional=('-movflags' '+faststart' "-preset $preset" "-tune $tune" "$profile" '-c:v' "libx264" '-b:v' "$bitrate")
+if [[ -z $framesv ]]; then
+  additional=( '-t' "$totalTime"  '-movflags' '+faststart' "-preset $preset" "-tune $tune" "$profile" '-c:v' "libx264" '-b:v' "$bitrate")
+else
+  additional=( '-frames:v' "$framesv" '-movflags' '+faststart' "-preset $preset" "-tune $tune" "$profile" '-c:v' "libx264" '-b:v' "$bitrate")
+fi
+
+# additional=( '-ss' "$seekToSecond"  '-movflags' '+faststart' "-preset $preset" "-tune $tune" "$profile" '-c:v' "libx264" '-b:v' "$bitrate")
+# additional=( '-ss' "$seekToSecond" '-frames:v' "$framesv" '-movflags' '+faststart' "-preset $preset" "-tune $tune" "$profile" '-c:v' "libx264" '-b:v' "$bitrate")
+# additional=( '-ss' "$seekToSecond" '-t' "$totalTime" '-movflags' '+faststart' "-preset $preset" "-tune $tune" "$profile" '-c:v' "libx264" '-b:v' "$bitrate")
 spamming=("-hide_banner" "-loglevel $loglevel" '-stats -y')
 # afterInput='-preset '"$preset"' -tune '"$tune"' '"$additional"' '"$profile"' -c:v libx264 -b:v '"$bitrate"
 # audio=('-c:a libfdk_aac' '-b:a 128k')
 audio=('-an')
 
 echo $outputVideo
-ffmpeg ${spamming[@]} -i "$1" ${additional[@]} -vf "[in]${filters[@]}[out]"  -pass 1  -an -f mp4 /dev/null
-ffmpeg ${spamming[@]} -i "$1" ${additional[@]} -vf "[in]${filters[@]}[out]"  -pass 2  ${audio[@]} "$outputVideo"
+ffmpeg ${spamming[@]} ${start[@]} -i "$1" ${additional[@]} -vf "[in]${filters[@]}[out]"  -pass 1  -an -f mp4 /dev/null
+ffmpeg ${spamming[@]} ${start[@]} -i "$1" ${additional[@]} -vf "[in]${filters[@]}[out]"  -pass 2  ${audio[@]} "$outputVideo"
 
 
 
